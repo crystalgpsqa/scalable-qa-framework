44e0fe3df65448f026c4888c8def67bf3eed0b1d
crystalgpsqa <crystalgps.qa@gmail.com>
2026-05-06 23:36:17 +0900

    chore: move credentials to env variables

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+USERNAME = os.getenv("QA_USERNAME")
+PASSWORD = os.getenv("QA_PASSWORD")
+
 BASE_URL = "https://global.oliveyoung.com/"
 LOGIN_URL = "https://global.oliveyoung.com/member/login"
 
